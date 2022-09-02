203157c0f3454457e5d1dc49f6a3282777c4fbef
Kogoon <gojs712@gmail.com>
2022-09-02 12:31:45 +0900

    Test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#from elasticapm.contrib.flask import  ElasticAPM
 from flask import Flask, jsonify, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -6,6 +7,27 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+apm = ElasticAPM(app)
+
+# or configure to use ELASTIC_APM in your application's settings
+from elasticapm.contrib.flask import ElasticAPM
+app.config['ELASTIC_APM'] = {
+# Set the required service name. Allowed characters:
+# a-z, A-Z, 0-9, -, _, and space
+#'SERVICE_NAME': 'unknown-python-service',
+'SERVICE_NAME': 'python-service',
+
+# Use if APM Server requires a secret token
+'SECRET_TOKEN': 'asdfqwer1234',
+
+# Set the custom APM Server URL (default: http://localhost:8200)
+'SERVER_URL': 'http://localhost:8200',
+
+# Set the service environment
+'ENVIRONMENT': 'production',
+}
+
+apm = ElasticAPM(app)
 
 
 class Todo(db.Model):
